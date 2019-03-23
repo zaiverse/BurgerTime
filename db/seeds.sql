b1632672ffb31dfecf57ac0378ff13d066c88a7e
zaiverse <brenda.garcia007@mymdc.net>
2019-03-23 00:35:30 -0400

    adding files and folders

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgerName) VALUES ('Cheese Burger');
+INSERT INTO burgers (burgerName) VALUES ('Meat Lovers');
+INSERT INTO burgers (burgerName) VALUES ('Veggie Bean Burger');
---
+++ server.js
@@ -0,0 +1,1 @@
+var experess = require("express");
