b4baed30764b22faade8d30749fd0708298eec3c
zaiverse <brenda.garcia007@mymdc.net>
2019-03-26 19:22:56 -0400

    unable to complete

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,7 @@ var router = express.Router();
 router.get("/", function(req, res) {
     burger.all(function(data) {
       var hbsObject = {
-        burger: data
+        burgers: data
       };
       console.log(hbsObject);
       res.render("index", hbsObject);
@@ -15,9 +15,9 @@ router.get("/", function(req, res) {
   
   router.post("/api/burgers", function(req, res) {
     burger.create([
-      "burger", "devoured"
+      "name", devoured
     ], [
-      req.body.name, req.body.sleepy
+      req.body.name, false
     ], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
@@ -30,7 +30,7 @@ router.get("/", function(req, res) {
     console.log("condition", condition);
   
     burger.update({
-      sleepy: req.body.sleepy
+      devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
@@ -44,7 +44,7 @@ router.get("/", function(req, res) {
   router.delete("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
   
-    cat.delete(condition, function(result) {
+    burger.delete(condition, function(result) {
       if (result.affectedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
---
+++ public/assests/js/javascript.js
@@ -0,0 +1,45 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".devour").on("click", function(event) {
+      var id = $(this).data("id");
+      var devourState = $(this).data("devour");
+  
+      var devour = {
+        devoured: devourState
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/cats/" + id, {
+        type: "PUT",
+        data: devour
+      }).then(
+        function() {
+          console.log("changed sleep to", devourState);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/cats", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new cat");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
